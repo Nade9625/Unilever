cf9c9e3aa148d1c57ce3bd37ad88d68680108d0d
Q-J-Py <abc@gmail.com>
2025-09-06 09:05:18 -1000

    commit 2

--- com.crm.Unilever/src/test/java/com/crm/Unilever/Organization/DeleteOrganization.java
+++ com.crm.Unilever/src/test/java/com/crm/Unilever/Organization/DeleteOrganization.java
@@ -1,5 +1,12 @@
 package com.crm.Unilever.Organization;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class DeleteOrganization {
+	@Test
+	public void delete() {
+		Reporter.log("org deleted",true);
+	}
 
 }
